ed89da02c469c2a398ec8efdd003d2edb8335dd0
Majd Taweel <42496516+majdtaweel@users.noreply.github.com>
2020-08-11 16:14:50 +0300

    Bug fix: Wait for the first user value to be initialized.

--- src/app/components/admin/admin.guard.ts
+++ src/app/components/admin/admin.guard.ts
@@ -3,6 +3,7 @@ import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from 
 import {Observable} from 'rxjs';
 import {UserService} from '../../services/user/user.service';
 import {Role} from '../../../models/user.model';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,8 @@ export class AdminGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.userService.hasAnyAuthority(Role.ADMIN.toString());
+    return this.userService.isUserInitialized.pipe(map(_ => {
+      return this.userService.hasAnyAuthority(Role.ADMIN.toString());
+    }));
   }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+import {BehaviorSubject, Observable, of, Subject, Subscription} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {map, tap} from 'rxjs/operators';
 import {AuthService} from '../auth/auth.service';
@@ -12,13 +12,30 @@ import {Role, User} from '../../../models/user.model';
 export class UserService {
   private user = new BehaviorSubject<User>(null);
   private userSubscription: Subscription;
+  private userInitialized = new Subject<User>();
+  private userDidInitialize = false;
 
   constructor(private authService: AuthService, private httpClient: HttpClient) {
     this.authService.tokenChanges.pipe(tap(username => {
       username = username?.length ? username : localStorage.getItem('username');
       console.log('JWT changed, username:', username);
       if (username?.length) {
-        this.retrieveUser(username);
+        if (this.userSubscription) {
+          this.userSubscription.unsubscribe();
+        }
+        this.userSubscription = this.retrieveUser(username).subscribe(user => {
+          if (!this.userDidInitialize) {
+            this.userInitialized.next(user);
+          }
+        });
+      } else {
+        if (!this.userDidInitialize) {
+          this.userInitialized.next(null);
+        }
+        if (this.userSubscription) {
+          this.userSubscription.unsubscribe();
+          this.userSubscription = null;
+        }
       }
     })).subscribe();
   }
@@ -52,20 +69,24 @@ export class UserService {
     return this.httpClient.put<User>(`${environment.apiURL}users`, user);
   }
 
-  private retrieveUser(username: string): void {
+  get isUserInitialized(): Observable<User> {
+    return this.userDidInitialize ? this.user.asObservable() : this.userInitialized.asObservable();
+  }
+
+  private retrieveUser(username: string): Observable<User> {
     if (this.authService.isAuthenticated()) {
       if (this.userSubscription) {
         this.userSubscription.unsubscribe();
       }
-      this.userSubscription = this.httpClient.get<any>(`${environment.apiURL}users/${username}`)
+      return this.httpClient.get<any>(`${environment.apiURL}users/${username}`)
         .pipe(tap(user => {
           this.user.next(user);
           console.log(user);
-        }))
-        .subscribe();
+        }));
     } else {
       this.user.next(null);
       console.log(`User ${username} is not logged in`);
+      return of(null);
     }
   }
 }
