991fa1d648b27f1f9e58dc3c62ee6d5603bd2dbc
Majd Taweel <42496516+majdtaweel@users.noreply.github.com>
2020-08-15 20:50:12 +0300

    Creating Release Service with the functionality of creating and getting releases.

---
+++ src/app/services/release/release.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Release} from '../../models/release.model';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReleaseService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  createRelease(release: Release): Observable<Release> {
+    return this.httpClient.post<Release>(`${environment.apiURL}releases`, release);
+  }
+
+  getReleases(): Observable<Release[]> {
+    return this.httpClient.get<Release[]>(`${environment.apiURL}releases`);
+  }
+}
