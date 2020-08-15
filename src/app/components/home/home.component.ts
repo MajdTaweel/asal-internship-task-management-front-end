96b754472fd3a63132d05a1b0cc41379543e04a6
Majd Taweel <42496516+majdtaweel@users.noreply.github.com>
2020-08-15 20:54:49 +0300

    Adding basic releases CRUD UI in home page (unfinished).

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../services/user/user.service';
+import {ReleaseService} from '../../services/release/release.service';
+import {Release, ReleaseStatus} from '../../models/release.model';
+import {Observable} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+import {MatDialog} from '@angular/material/dialog';
+import {Role} from '../../models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +13,44 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  releases: Observable<Release[]>;
 
-  constructor() {
+  constructor(
+    private userService: UserService,
+    private releaseService: ReleaseService,
+    private dialog: MatDialog,
+  ) {
   }
 
   ngOnInit(): void {
+    this.releases = this.getReleases();
   }
 
+  onDisplayReleaseEditDialog(): void {
+    this.releaseService.createRelease(new Release(
+      null,
+      'New Release',
+      'Test Release',
+      (new Date()),
+      ReleaseStatus.NEW,
+    )).subscribe(release => {
+      console.log('Created release', release);
+      this.releases = this.getReleases();
+    });
+  }
+
+  isReleaseOwnerOrAdmin(createdBy: string): Observable<boolean> {
+    return this.userService.currentUser
+      .pipe(map(user => this.userService.hasAnyAuthority(Role.ADMIN) || user.login === createdBy));
+  }
+
+  onEditRelease(id: string): void {
+  }
+
+  onDeleteRelease(id: string): void {
+  }
+
+  private getReleases(): Observable<Release[]> {
+    return this.releaseService.getReleases().pipe(tap(releases => console.log('My releases', releases)));
+  }
 }
