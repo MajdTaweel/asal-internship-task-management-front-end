6e9cf11a85c46819f83f55273da1de7a97a75c1d
Majd Taweel <42496516+majdtaweel@users.noreply.github.com>
2020-08-16 09:34:28 +0300

    Refactoring and adding delete confirmation logic.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,56 +1,85 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from '../../services/user/user.service';
 import {ReleaseService} from '../../services/release/release.service';
-import {Release, ReleaseStatus} from '../../models/release.model';
-import {Observable} from 'rxjs';
-import {map, tap} from 'rxjs/operators';
+import {Release} from '../../models/release.model';
+import {Observable, of, Subscription} from 'rxjs';
+import {switchMap, tap} from 'rxjs/operators';
 import {MatDialog} from '@angular/material/dialog';
-import {Role} from '../../models/user.model';
+import {AlertService} from '../../services/alert/alert.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   releases: Observable<Release[]>;
+  subscriptions = new Subscription();
 
   constructor(
     private userService: UserService,
     private releaseService: ReleaseService,
     private dialog: MatDialog,
+    private alertService: AlertService,
   ) {
   }
 
   ngOnInit(): void {
-    this.releases = this.getReleases();
+    this.updateReleasesList();
   }
 
   onDisplayReleaseEditDialog(): void {
-    this.releaseService.createRelease(new Release(
-      null,
-      'New Release',
-      'Test Release',
-      (new Date()),
-      ReleaseStatus.NEW,
-    )).subscribe(release => {
-      console.log('Created release', release);
-      this.releases = this.getReleases();
-    });
+    // this.releaseService.createRelease(new Release(
+    //   null,
+    //   'New Release',
+    //   'Test Release',
+    //   (new Date()),
+    //   ReleaseStatus.NEW,
+    // )).subscribe(release => {
+    //   console.log('Created release', release);
+    //   this.updateReleasesList();
+    // });
   }
 
   isReleaseOwnerOrAdmin(createdBy: string): Observable<boolean> {
-    return this.userService.currentUser
-      .pipe(map(user => this.userService.hasAnyAuthority(Role.ADMIN) || user.login === createdBy));
+    return this.releaseService.isReleaseOwnerOrAdmin(createdBy);
   }
 
   onEditRelease(id: string): void {
   }
 
   onDeleteRelease(id: string): void {
+    const sub = this.checkConfirmationThenDelete(id).subscribe();
+    this.subscriptions.add(sub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   private getReleases(): Observable<Release[]> {
     return this.releaseService.getReleases().pipe(tap(releases => console.log('My releases', releases)));
+  }
+
+  private checkConfirmationThenDelete(id: string): Observable<null> {
+    return this.alertService
+      .displayDeleteConfirmationDialog('Release will be permanently deleted. Continue deletion?')
+      .pipe(switchMap(confirmed => {
+        if (confirmed) {
+          return this.deleteRelease(id);
+        }
+        return of(null);
+      }));
+  }
+
+  private deleteRelease(id: string): Observable<null> {
+    return this.releaseService.deleteRelease(id).pipe(tap(_ => {
+      this.updateReleasesList();
+      console.log(`Release with id: ${id} deleted`);
+    }));
+  }
+
+  private updateReleasesList(): void {
+    this.releases = this.getReleases();
   }
 }
