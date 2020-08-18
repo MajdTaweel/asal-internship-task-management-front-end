217bd02ca87bd973f2162599c0ae9bbb2fbc9531
Majd Taweel <42496516+majdtaweel@users.noreply.github.com>
2020-08-18 09:00:19 +0300

    Fixing getTasksByReleaseId method definition and adding ownership or admin check logic.

--- src/app/services/task/task.service.ts
+++ src/app/services/task/task.service.ts
@@ -3,17 +3,20 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Observable} from 'rxjs';
 import {Task} from '../../models/task.model';
+import {map} from 'rxjs/operators';
+import {Role} from '../../models/user.model';
+import {UserService} from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private userService: UserService) {
   }
 
-  getTasksByReleaseId(releaseId: string): Observable<Task> {
-    return this.httpClient.get<Task>(`${environment.apiURL}release/${releaseId}/tasks`);
+  getTasksByReleaseId(releaseId: string): Observable<Task[]> {
+    return this.httpClient.get<Task[]>(`${environment.apiURL}release/${releaseId}/tasks`);
   }
 
   createTask(task: Task): Observable<Task> {
@@ -26,5 +29,10 @@ export class TaskService {
 
   deleteTask(id: string): Observable<null> {
     return this.httpClient.delete<null>(`${environment.apiURL}tasks/${id}`);
+  }
+
+  isTaskOwnerOrAdmin(createdBy: string): Observable<boolean> {
+    return this.userService.currentUser
+      .pipe(map(user => this.userService.hasAnyAuthority(Role.ADMIN) || user?.login === createdBy));
   }
 }
